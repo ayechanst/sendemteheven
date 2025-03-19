cea6b0ac5e1aca7bd20f88c6333967b30550f676
ayechan <ayechan.sant@gmail.com>
2025-03-19 12:46:57 -0700

    still only bottom layer rendering

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,23 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(TilemapPlugin)
         .add_plugins(TiledMapPlugin::default())
+        .add_plugins(PlayerPlugin)
         .add_systems(Startup, setup)
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // let map_handle: Handle<TiledMap> = asset_server.load("test2.tmx");
-    commands.spawn((
-        TiledMapHandle(asset_server.load("test2.tmx")),
-        TiledMapAnchor::Center,
-    ));
+    let map_handle: Handle<TiledMap> = asset_server.load("test2.tmx");
+
+    // commands.spawn((TiledMapHandle(map_handle), TiledMapAnchor::Center));
+    for layer_index in 0..3 {
+        commands.spawn((
+            TiledMapHandle(map_handle.clone()),
+            TiledMapLayer,
+            Transform::from_xyz(0.0, 0.0, layer_index as f32),
+            Visibility::Visible,
+            TiledMapAnchor::Center,
+        ));
+    }
     commands.spawn(Camera2d);
 }
